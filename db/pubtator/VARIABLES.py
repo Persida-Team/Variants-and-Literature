0290c1015e72d29feaa85102a6d13ae5b739b674
Novak <novak.martinovic@apisassay.com>
2025-10-22 15:56:43 +0200

    fixed syntax error on import

--- db/pubtator/VARIABLES.py
+++ db/pubtator/VARIABLES.py
@@ -1,7 +1,6 @@
 import os
 from contextlib import contextmanager
 
-from dontenv import load_dotenv
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
